b7d44bd42880cba6becccd17dc1f328ad0d03115
Juanver <juan99verano@gmail.com>
2023-01-13 22:45:22 -0500

    feat: Update navbar styles,  add new assets and add spacing rem enum to styles `spacing` `size`

---
+++ src/assets/spacing/Spacing.ts
@@ -0,0 +1,8 @@
+export enum Spacing {
+  Spacing0 = "0",
+  Spacing1 = "0.4rem",
+  Spacing2 = "0.8rem",
+  Spacing3 = "1.2rem",
+  Spacing4 = "1.6rem",
+  Spacing5 = "2rem",
+}
--- src/main.styled.ts
+++ src/main.styled.ts
@@ -1,10 +1,11 @@
 import { createGlobalStyle } from "styled-components";
+import { Spacing } from "./assets/spacing/Spacing";
 
 export const GlobalStyle = createGlobalStyle`
   body {
     font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-    font-size: 16px;
-    line-height: 24px;
+    font-size: ${Spacing.Spacing2};
+    line-height: ${Spacing.Spacing3};
     margin: 0;
   }
 `;
