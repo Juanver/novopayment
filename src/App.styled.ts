1258c7f77687e01d1702a47b17665ae165155f42
Juanver <juan99verano@gmail.com>
2023-01-14 15:29:31 -0500

    feat: add footer and update app styles

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 import { Palette } from "./assets/palette/Palette";
+import bgContent from "./assets/bg-content.jpg"
 
 const ContainerApp = styled.div``;
 
 const ContentApp = styled.div`
-  background-color: ${Palette.BrandGray100};
+  background-image: url(${bgContent});
+  background-repeat: repeat-x;
+  height: calc(100vh - 200px);
 `;
 
 export const StyledApp = {
